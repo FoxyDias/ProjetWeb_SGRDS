6e048dbdaa9f3be41ae174380ec7d50d4fa5157c
FoxyDias <ludiwebjeux@hotmail.fr>
2024-01-30 10:56:47 +0100

    je sais plus ce que j'ai fait mais y'a un jolie truc dans le header

--- app/Views/communs/enTete.php
+++ app/Views/communs/enTete.php
@@ -9,4 +9,27 @@
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
     <link rel="stylesheet" href="/output.css">
 </head>
+
 <body>
+<header class="relative bg-white left-0 p-4">
+
+    <div class="flex justify-between items-center">
+        <img src="/img/logoDepInformatique.gif" alt="Département informatique" class="h-9 md:h-16 lg:h-18">
+
+            <!-- si admin connecté -->
+            <?php if (session()->get('estAdmin')): ?>
+                <div class="flex items-center rounded-[12px] bg-gray-500 p-2 md:p-4 h-9 md:h-16 lg:h-18 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-gray-300 duration-300">
+                <img src="/img/person-svgrepo-com.png" alt="Département informatique" class="w-10 h-10">
+                <span class="text-sm text-gray-800 ml-2"><? echo $nomUtilisateur ?></span>
+            <?php else: ?>
+                <a href="/connexion">
+                    <div class="flex items-center rounded-[12px] bg-gray-500 p-2 md:p-4 h-9 md:h-16 lg:h-18 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-gray-300 duration-300">
+                    <p class="text-sm text-gray-800 ml-2">Connexion</p>
+                </a>
+            <?php endif; ?>
+
+        </div>
+    </div>
+
+</header>
+
