a85c5809845863b1ab4bf42a5f0dc17dfc3523e8
Noemie Claccin <noemie.claccin@gmail.com>
2024-02-01 11:08:55 +0100

    ajout route traitement données ajout de rattrapage DS

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,6 +18,9 @@ $routes->get('resetmdp/(:any)', 'ResetMdpController::index/$1');
 $routes->post('resetmdp/(:any)', 'ResetMdpController::resetMdp/$1');
 
 $routes->get('listerattrapages', 'ListeRattrapagesController::index');
+
 $routes->get('ajoutrattrapage/(:any)', 'AjoutRattrapageController::index/$1');
+$routes->post('ajoutrattrapage/(:any)/traitement', 'AjoutRattrapageController::traitement/$1');
+
 
 $routes->get('ajout_etudiants_absents', 'AjoutElevesAbsents::index');
