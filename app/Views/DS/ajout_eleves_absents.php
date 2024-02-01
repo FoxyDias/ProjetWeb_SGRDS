3d1c63d488320664ec07b26df1e99665b7c03813
Noemie Claccin <noemie.claccin@gmail.com>
2024-02-01 16:43:21 +0100

    style retour

--- app/Views/DS/ajout_eleves_absents.php
+++ app/Views/DS/ajout_eleves_absents.php
@@ -55,10 +55,8 @@ $enseignant = "Philippe LEPIVERT";
 
         <!--Flèche de retours -->
         <div class="inline-block">
-            <a href="./ajoutds" class="text-gray-500 hover:text-gray-700">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
-                </svg>
+            <a href="./ajoutds">
+                <img src="<?= base_url('/images/fleche_retour.png'); ?>"  >
             </a>
         </div>
 
